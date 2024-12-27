c7cbb90b2a5a37916a3a296afda6a4f4066dc47b
Arman Rajib <ahrajib6789@gmail.com>
2024-12-28 01:15:13 +0600

    Create the root-layout

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
-import Navigation from './_components/Navigation';
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to Paradise.</h1>
     </div>
   );
