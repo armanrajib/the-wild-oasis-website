809c7104e7a7cff58cede992881d83a627c69354
Arman Rajib <ahrajib6789@gmail.com>
2024-12-28 03:23:04 +0600

    Set up a clean project structure

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,20 +1,7 @@
-import Counter from '../_components/Counter';
-
-export default async function Page() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-
+export default function Page() {
   return (
     <div>
       <h1>All the Available Cabins</h1>
-
-      <ol>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ol>
-
-      <Counter users={data} />
     </div>
   );
 }
