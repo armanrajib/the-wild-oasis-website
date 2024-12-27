8ca3d623404e993b6a389c09b9e5a3f25f35d2a0
Arman Rajib <ahrajib6789@gmail.com>
2024-12-28 02:04:53 +0600

    Fetch data in a page (server component)

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,15 @@
-export default function Page() {
-  return <h1>All the Available Cabins</h1>;
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return (
+    <div>
+      <h1>All the Available Cabins</h1>
+      <ol>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
 }
