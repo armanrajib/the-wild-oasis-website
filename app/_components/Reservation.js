ef5e1de06a696aa66928d1fd467e49688231851f
Arman Rajib <ahrajib6789@gmail.com>
2024-12-30 15:12:59 +0600

    Get the user session, and update the necessary components (Navigation, Reservation, ReservationForm, and LoginMessage) who relies on the session data

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,16 @@
 import DateSelector from '@/app/_components/DateSelector';
 import ReservationForm from '@/app/_components/ReservationForm';
+import LoginMessage from '@/app/_components/LoginMessage';
 import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service';
+import { auth } from '@/app/_lib/auth';
 
 export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
 
   return (
     <div className="flex flex-col gap-10 border border-primary-800 min-h-[400px]">
@@ -15,8 +19,14 @@ export default async function Reservation({ cabin }) {
         settings={settings}
         bookedDates={bookedDates}
       />
-      <div className="scale-[.99]">
-        <ReservationForm cabin={cabin} />
+      <div>
+        {session?.user ? (
+          <div className="scale-[.99]">
+            <ReservationForm cabin={cabin} user={session.user} />
+          </div>
+        ) : (
+          <LoginMessage />
+        )}
       </div>
     </div>
   );
