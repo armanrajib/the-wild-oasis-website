a0f89c32fa24175d6cab5bf2b412ebb200d98d13
Arman Rajib <ahrajib6789@gmail.com>
2024-12-30 23:44:21 +0600

    Create a new guest on first signin in the supabase database, and get guestId in the session data

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -2,6 +2,8 @@ import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 // import Credentials from 'next-auth/providers/credentials';
 
+import { createGuest, getGuest } from '@/app/_lib/data-service';
+
 const authConfig = {
   providers: [
     Google({
@@ -23,6 +25,23 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
