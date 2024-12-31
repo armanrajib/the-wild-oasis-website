3c5c6a30abefa97f5b85e1cb478640c072a909e5
Arman Rajib <ahrajib6789@gmail.com>
2024-12-31 19:18:21 +0600

    Update a reservation, and reconfigure the SubmitButton

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,11 +1,13 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 import { auth, signIn, signOut } from '@/app/_lib/auth';
 import {
   deleteBooking,
   getBookings,
+  updateBooking,
   updateGuest,
 } from '@/app/_lib/data-service';
 
@@ -52,4 +54,39 @@ export async function deleteReservationAction(bookingId) {
   await deleteBooking(bookingId);
 
   revalidatePath('/account/reservations');
+}
+
+// Update a reservation
+export async function updateReservationAction(formData) {
+  // 1) Authentication
+  const session = await auth();
+
+  if (!session)
+    throw new Error('You must be signed in to update a reservation');
+
+  // 2) Authorization
+  const bookingId = formData.get('bookingId');
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(Number(bookingId)))
+    throw new Error('You are not authorized to update this reservation');
+
+  // 3) Validation
+  const numGuests = formData.get('numGuests');
+  const observations = formData.get('observations').slice(0, 1000);
+
+  if (!numGuests || !observations)
+    throw new Error('Please provide the number of guests and any observations');
+
+  // 4) Building the updated data and updating the reservation
+  const updatedData = { numGuests, observations };
+  await updateBooking(bookingId, updatedData);
+
+  // 5) Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath('/account/reservations');
+
+  // 6) Redirecting
+  redirect('/account/reservations');
 }
